9cccef8f91fc1e85a78d7f4ea191bdeba46d0469
zynn <joao.alduan@gmail.com>
2024-10-04 19:51:07 -0500

    feat: add customization on messages

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Client, LocalAuth, Events } from "whatsapp-web.js";
 import * as qrcode from "qrcode-terminal";
 
 import handleMessageCreate from "./messages";
+import { authenticated } from "../lang.json";
 
 const client = new Client({
     authStrategy: new LocalAuth({ clientId: "kyu-bot", dataPath: ".wwebjs_data" }),
@@ -12,7 +13,7 @@ const client = new Client({
 });
 
 client.on(Events.QR_RECEIVED, (qr) => qrcode.generate(qr, { small: true }));
-client.on(Events.AUTHENTICATED, () => console.log("Autenticado com sucesso!"));
+client.on(Events.AUTHENTICATED, () => console.log(authenticated));
 
 client.on(Events.MESSAGE_CREATE, (message) => 
     handleMessageCreate(client, message));
--- src/messages.ts
+++ src/messages.ts
@@ -1,12 +1,14 @@
 import { Client, Message } from "whatsapp-web.js";
+import { format } from "util";
 
-const WELCOME_MESSAGE = `
-    Olá! Eu sou o Kyu, um bot pra te fazer figurinhas! 😊
-
-    - Para fazer uma figurinha, basta me enviar uma imagem! 🖼️
-
-    Feito com carinho por @zuukynn! ❤️
-`.trim().replace(/ {2,}/g, "");
+import { 
+    welcomeMessage, 
+    stickerName, 
+    stickerAuthor, 
+    createdStickerLog, 
+    errorStickerLog, 
+    videoUnsupported 
+} from "../lang.json";
 
 export default async function handleMessageCreate(client: Client, message: Message) {
     if (message.fromMe) return;
@@ -15,7 +17,7 @@ export default async function handleMessageCreate(client: Client, message: Messa
     const messages = await chat.fetchMessages({ fromMe: true });
 
     if (messages.length == 0) {
-        await client.sendMessage(message.from, WELCOME_MESSAGE);
+        await client.sendMessage(message.from, welcomeMessage.join("\n"));
     }
 
     switch (message.type) {
@@ -38,20 +40,20 @@ async function handleImageMessage(message: Message) {
         
         await message.reply(media, undefined, {
             sendMediaAsSticker: true,
-            stickerName: "Figurinha feita por",
-            stickerAuthor: "@zuukynn/kyu-bot"
+            stickerName,
+            stickerAuthor
         });
         await message.react("✅");
 
-        console.log(`[INFO] ${message.from} criou uma figurinha!`);
+        console.log(format(createdStickerLog, message.from));
     } catch (error) {
         await message.react("⛔");
 
-        console.error(`[INFO] ${message.from} obteve um erro!`, error);
+        console.error(format(errorStickerLog, message.from), );
     }
 }
 
 async function handleVideoMessage(message: Message) {
     await message.react("❌");
-    await message.reply("Desculpe, mas eu não consigo fazer figurinhas de vídeos! 😔");
+    await message.reply(videoUnsupported);
 }
